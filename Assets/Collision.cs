c9f068389a7bfc33ccf54e57cd4d6ae2e56ad82e
Alex <alex.agaf95@gmail.com>
2016-12-04 22:16:00 -0500

    Init commit

---
+++ Assets/Collision.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour {
+
+    public ParticleSystem dust;
+    public Rigidbody rb;
+
+    void Start () {
+	    rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag != "Sphere")
+            return;
+
+        float power = 50.0F;
+        float radius = 100.0F;
+
+        rb.isKinematic = false;
+        Vector3 explosionPos;
+        explosionPos.y = 0;
+        explosionPos.x = 0;
+        explosionPos.z = 0;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(power, explosionPos, radius, 1);
+            }
+
+        }
+    }
+}
---
+++ Assets/Movements.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movements : MonoBehaviour {
+    public int speed = 10;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+        }
+        
+    
+
+        
+
+
+    }
+}
